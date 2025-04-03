1401a31b31fa56decf9e9a5cdc76577e36753824
jiyoungnim <cloundyon31@gmail.com>
2025-04-03 17:04:48 +0900

    ✨ 400. Sending Signup Requests From the Frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,32 +2,38 @@ import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
-  const data = req.body;
-  const { email, password } = data;
+  // if (req.method !== "POST ") {
+  //   return;
+  // }
 
-  if (
-    !email ||
-    !email.includes("@") ||
-    !password ||
-    !password.trim().length < 7
-  ) {
-    res.status(422).json({
-      message:
-        "Invalid input - password should also be at leasy 7 characters long.",
+  if (req.method === "POST") {
+    const data = req.body;
+    const { email, password } = data;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      !password.trim().length > 7
+    ) {
+      res.status(422).json({
+        message:
+          "Invalid input - password should also be at least 7 characters long.",
+      });
+      return;
+    }
+
+    const client = await connectToDatabase();
+    const db = client.db("urang-market");
+
+    const hashedPassword = hashPassword(password);
+
+    db.collection("users").insertOne({
+      email,
+      password: hashedPassword,
     });
-    return;
+
+    res.status(201).json({ message: "Created User!" });
   }
-
-  const client = await connectToDatabase();
-  const db = client.db();
-
-  const hashPassword = hashPassword(password);
-
-  db.collection("users").insertOne({
-    email,
-    password: hashPassword,
-  });
-
-  res.status(201).json({ message: "Created User!" });
 }
 export default handler;
